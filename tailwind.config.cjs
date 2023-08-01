4cf91dfe70c2e589258d5eec14fdc5256045b4ab
AMIV <arthurmodymaniv@gmail.com>
2023-07-31 23:58:31 -0700

    ham icon complete, colors reoriented, center graphic works, need logo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["wireframe", "business", "light", "dark"]
+    themes: ["wireframe", "business", "light", "dark", "luxury"]
   }
 }
